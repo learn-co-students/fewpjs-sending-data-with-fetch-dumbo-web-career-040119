35bd2e47cb0fcde3b865e97de8796bd93d9b8e37
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-05-15 17:35:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName, userEmail){
+  const userObject = {
+    name: userName,
+    email: userEmail
+  }
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userObject)
+  }
+
+  return fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log("object: ", object)
+      document.body.innerHTML = object.id;
+    })
+    .catch(function(error) {
+      console.log("error: ", error.message)
+      document.body.innerHTML = error.message;
+    })
+}
