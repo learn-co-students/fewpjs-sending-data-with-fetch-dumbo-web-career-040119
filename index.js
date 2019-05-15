e6b3c56bd1670b3ff1ebe674e65e14735af78dc7
lsamano <leizl.samano@flatironschool.com>
2019-05-15 13:07:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+const submitData = () => {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: "Steve",
+      email: "steve@steve.com"
+    })
+  })
+  .then(res => res.json())
+  .then(data => document.body.append(data.id))
+  .catch(error => document.body.append(error.message))
+}
