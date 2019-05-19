c2c70ec796c35420ecadcf2b5c3c66b878c18078
Adam Kearney <aklives@hotmail.com>
2019-05-19 19:28:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const submitData = function(username, email){
+  let formData = {
+    name: username,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      document.body.innerHTML = object["id"]
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message
+    })
+
+}
